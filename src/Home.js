43e4667447323489109165863ff807f8c7904a70
Alwazzan <a7laaroka2013@gmail.com>
2022-05-11 23:46:34 +0300

    adding more logic

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,20 @@
+import { useState } from "react";
+
 const Home = () => {
+    const [name, setName] = useState('mario');
+    const handleClick = () => {
+        setName('luigi');
+    }
+
+    const handleClickAgain = (name, e) => {
+        console.log('Hi ' + name, e.target);
+    }
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <p>{name}</p>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={(e) => handleClickAgain('rock', e)}>Click me 2nd</button>
         </div>
     );
 }
