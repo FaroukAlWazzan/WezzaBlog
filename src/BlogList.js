025547cbc74e53316d382557c3cc79cdf69fb8af
Alwazzan <a7laaroka2013@gmail.com>
2022-05-12 17:01:08 +0300

    commenting the refactored code

---
+++ src/BlogList.js
@@ -0,0 +1,17 @@
+const BlogList = ({ blogs, title, handleDelete }) => {
+    // const blogs = props.blogs;
+    return (
+        <div className="blog-list">
+            {title}
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by: {blog.author}</p>
+                    {/* <button onClick={() => handleDelete(blog.id)}>delete blog</button> */}
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default BlogList;
--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,43 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
-    const [name, setName] = useState('mario');
-    const handleClick = () => {
-        setName('luigi');
-    }
+    // const [name, setName] = useState('mario');
+    // const [age, setAge] = useState(25);
+    // const handleClick = () => {
+    //     setName('luigi');
+    //     setAge(29);
+    // }
+    const [blogs, setBlogs] = useState(null)
 
-    const handleClickAgain = (name, e) => {
-        console.log('Hi ' + name, e.target);
-    }
+    // const [name, setName] = useState('mario')
+    // const handleDelete = (id) => {
+    //     const newBlogs = blogs.filter((blog) => blog.id !== id);
+    //     setBlogs(newBlogs);
+    // }
+
+    useEffect(() => {
+        fetch('http://localhost:8000/blogs')
+            .then((res) => {
+                return res.json();;
+            })
+            .then(data => {
+                console.log(data);
+                setBlogs(data);
+            })
+    }, [])
+
+    // const handleClickAgain = (name, e) => {
+    //     console.log('Hi ' + name, e.target);
+    // }
     return (
         <div className="home">
-            <h2>Homepage</h2>
-            <p>{name}</p>
-            <button onClick={handleClick}>Click me</button>
-            <button onClick={(e) => handleClickAgain('rock', e)}>Click me 2nd</button>
+            {blogs && <BlogList blogs={blogs} title="all blogs" />}
+            {/* <h2>Homepage</h2> */}
+            {/* <p>{name} is {age} years old</p> */}
+            {/* <button onClick={() => setName('luigi')}>Click me</button> */}
+            {/* {name} */}
+            {/* <button onClick={(e) => handleClickAgain('rock', e)}>Click me 2nd</button> */}
         </div>
     );
 }
