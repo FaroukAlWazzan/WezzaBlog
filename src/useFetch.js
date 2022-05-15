f6f383dc571b15854154776ed41a3f8813fe1d5e
Alwazzan <a7laaroka2013@gmail.com>
2022-05-15 18:04:48 +0300

    putting the finishing touches

--- src/useFetch.js
+++ src/useFetch.js
@@ -10,30 +10,30 @@ const useFetch = (url) => {
     useEffect(() => {
         const abortCont = new AbortController();
 
-        setTimeout(() => {
-            fetch(url, { signal: abortCont.signal })
-                .then(res => {
-                    if (!res.ok) {
-                        throw Error('could not fetch that resource');
-                    }
-                    return res.json();;
-                })
-                .then(data => {
-                    // console.log(data);
+
+        fetch(url, { signal: abortCont.signal })
+            .then(res => {
+                if (!res.ok) {
+                    throw Error('could not fetch that resource');
+                }
+                return res.json();;
+            })
+            .then(data => {
+                // console.log(data);
+                setIsPending(false);
+                setData(data);
+                setError(null);
+            })
+            .catch(err => {
+                if (err.name === 'AbortError') {
+                    console.log('fetch aborted');
+                } else {
                     setIsPending(false);
-                    setData(data);
-                    setError(null);
-                })
-                .catch(err => {
-                    if (err.name === 'AbortError') {
-                        console.log('fetch aborted');
-                    } else {
-                        setIsPending(false);
-                        setError(err.message);
-                        console.log(err.message);
-                    }
-                })
-        }, 1000)
+                    setError(err.message);
+                    console.log(err.message);
+                }
+            })
+
         return () => abortCont.abort();
     }, [url])
 
