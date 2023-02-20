2e408613a1eeb44895e30925f1a6c183158cc064
Alwazzan <a7laaroka2013@gmail.com>
2023-02-20 22:41:09 +0300

    one line component properties modification

--- src/Create.js
+++ src/Create.js
@@ -28,23 +28,11 @@ const Create = () => {
       <h2>Add a New Blog</h2>
       <form onSubmit={handleSubmit}>
         <label>Blog Title:</label>
-        <input
-          type="text"
-          value={title}
-          onChange={e => setTitle(e.target.value)}
-          required
-        />
+        <input type="text" value={title} onChange={e => setTitle(e.target.value)} required />
         <label>Blog Body:</label>
-        <textarea
-          required
-          value={body}
-          onChange={e => setBody(e.target.value)}
-        ></textarea>
+        <textarea required value={body} onChange={e => setBody(e.target.value)}></textarea>
         <label>Blog Author:</label>
-        <select
-          value={author}
-          onChange={e => setAuthor(e.target.value)}
-        >
+        <select value={author} onChange={e => setAuthor(e.target.value)}>
           <option value="jack">Jack</option>
           <option value="ihsan">Ihsan</option>
         </select>
