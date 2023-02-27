8dd193b1502cf53f6d6cdda85f4e1966685ba95d
Alwazzan <a7laaroka2013@gmail.com>
2023-02-27 21:50:47 +0300

    blog details component edit

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -2,30 +2,30 @@ import { useNavigate, useParams } from "react-router-dom";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
-    const { id } = useParams();
-    const { data, isPending, error } = useFetch('http://localhost:8000/blogs/' + id);
-    const navigate = useNavigate();
+  const { id } = useParams();
+  const { data, isPending, error } = useFetch('http://localhost:8000/blogs/' + id);
+  const navigate = useNavigate();
 
-    const handleDelete = () => {
-        fetch('http://localhost:8000/blogs/' + id, { method: 'DELETE' }).then(() => {
-            navigate('/')
-        })
-    }
-    
-    return (
-        <div className="blog-details">
-            {isPending && <div>Loading...</div>}
-            {error && <div>{error}</div>}
-            {data && (
-                <article>
-                    <h2>{data.title}</h2>
-                    <p>Written by: {data.author}</p>
-                    <div>{data.body}</div>
-                    <button onClick={handleDelete}>Delete</button>
-                </article>
-            )}
-        </div>
-    );
+  const handleDelete = () => {
+    fetch('http://localhost:8000/blogs/' + id, { method: 'DELETE' }).then(() => {
+      navigate('/')
+    })
+  }
+
+  return (
+    <div className="blog-details">
+      {isPending && <div>Loading...</div>}
+      {error && <div>{error}</div>}
+      {data && (
+        <article>
+          <h2>{data.title}</h2>
+          <p>Written by: {data.author}</p>
+          <div>{data.body}</div>
+          <button onClick={handleDelete}>Delete</button>
+        </article>
+      )}
+    </div>
+  );
 }
 
 export default BlogDetails;
