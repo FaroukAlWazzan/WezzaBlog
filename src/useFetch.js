dfd81998d095dbd7de1d356915ca427459850b8e
Alwazzan <a7laaroka2013@gmail.com>
2023-03-01 21:01:04 +0300

    code clean up

--- src/useFetch.js
+++ src/useFetch.js
@@ -18,7 +18,6 @@ const useFetch = (url) => {
         return res.json();;
       })
       .then(data => {
-        // console.log(data);
         setIsPending(false);
         setData(data);
         setError(null);
