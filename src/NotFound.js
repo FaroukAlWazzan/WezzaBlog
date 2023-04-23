3cfb3d405bd2f44ff2d613dc00a1d078c6008f3a
Alwazzan <a7laaroka2013@gmail.com>
2023-04-23 20:08:36 +0300

    small fix and product update

--- src/NotFound.js
+++ src/NotFound.js
@@ -1,8 +1,7 @@
-// the 404 not found page component
+the 404 not found page component
 import { Link } from "react-router-dom";
 
 const NotFound = () => {
-
     return (
         <div className="not-found">
             <h2>Sorry</h2>
