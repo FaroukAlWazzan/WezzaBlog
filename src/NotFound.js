89271ed7fc0c0cc694a953049b228da9b7fd79d8
Alwazzan <a7laaroka2013@gmail.com>
2023-04-26 01:17:55 +0300

    index.css styles

--- src/NotFound.js
+++ src/NotFound.js
@@ -1,7 +1,8 @@
-the 404 not found page component
+// the 404 not found page component
 import { Link } from "react-router-dom";
 
 const NotFound = () => {
+    
     return (
         <div className="not-found">
             <h2>Sorry</h2>
