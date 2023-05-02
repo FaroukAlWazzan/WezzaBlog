165f47c0f50cb4d083e425f82d521a02d71b2936
Alwazzan <a7laaroka2013@gmail.com>
2023-05-02 23:19:26 +0300

    fixed some spaces with notfound component

--- src/NotFound.js
+++ src/NotFound.js
@@ -2,14 +2,13 @@
 import { Link } from "react-router-dom";
 
 const NotFound = () => {
-    
-    return (
-        <div className="not-found">
-            <h2>Sorry</h2>
-            <p>We can't find this page (404) code error</p>
-            <Link to='/'>Go to Home Page</Link>
-        </div>
-    );
+  return (
+    <div className="not-found">
+      <h2>Sorry</h2>
+      <p>We can't find this page (404) code error</p>
+      <Link to='/'>Go to Home Page</Link>
+    </div>
+  );
 }
 
 export default NotFound;
